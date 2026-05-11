2c0db9b2a5157136552fc658c1b62a8d742e09ea
kamrul25 <kamrul.coder01@gmail.com>
2026-05-11 23:40:01 +0600

    All the REST API crud operation

--- app/new.py
+++ app/new.py
@@ -1,12 +1,50 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 new = FastAPI()
 # main_logic = FastAPI()
 
+# Define Item model
+class Item(BaseModel):
+    name: str
+    price: float
+
+# Temporary in-memory storage
+items ={}
+
 # @main_logic.get("/")
 @new.get('/')
 def root():
-    return{"Hi! How are you?"}
+    return{"name":"Fast APi","location":"From FastAPI"}
+
+# Create
+@new.post("/items/")
+async def create_item(item: Item):
+    item_id = len(items) + 1
+    items[item_id] = item
+    return {"id": item_id, "item": item}
+
+# Read
+@new.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return items.get(item_id,{"error":"Item not found"})
+
+
+# Update
+@new.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    if item_id in items:
+        items[item_id] = item
+        return {"message": "Item update", "item": item}
+    return {"error": "Item not found"}
+
+# Delete
+@new.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    if item_id in items:
+        deleted_item = items.pop(item_id)
+        return {"message": "Item Deleted", "item": deleted_item}
+    return {"error":"Item not found"}
 
 
 # To run the server use :
